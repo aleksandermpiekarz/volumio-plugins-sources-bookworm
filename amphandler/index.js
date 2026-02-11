83c77c05f41e0d34fc33b6bf631b78db9d060d6b
aleksander.piekarz <aleksander.m.piekarz@gmail.com>
2026-02-11 23:55:20 +0100

    remove unused parts of script

--- amphandler/index.js
+++ amphandler/index.js
@@ -1,8 +1,6 @@
 'use strict';
 
 var libQ = require('kew');
-var fs = require('fs-extra');
-var config = new (require('v-conf'))();
 var execFile = require('child_process').execFile;
 
 module.exports = amphandler;
@@ -11,40 +9,25 @@ function amphandler(context) {
     this.context = context;
     this.commandRouter = this.context.coreCommand;
     this.logger = this.context.logger;
-    this.configManager = this.context.configManager;
 
     this.irLock = false;
-    this.irCooldownMs = 80;
+    this.irCooldownMs = 120;
 
     this.irDevice = '/dev/lirc0';
     this.irCarrier = '36000';
+
+    this.irMap = {
+        power: 'power.ir',
+        volUp: 'vol_up.ir',
+        volDown: 'vol_down.ir',
+        linePhono: 'line_phono.ir',
+        lineCd: 'line_cd.ir',
+        lineOne: 'line_one.ir',
+        lineTwo: 'line_two.ir'
+    };
 }
 
 amphandler.prototype.onVolumioStart = function () {
-    var configFile = this.commandRouter.pluginManager.getConfigurationFile(this.context, 'config.json');
-    this.config = new (require('v-conf'))();
-    this.config.loadFile(configFile);
-
-    var dev = this.config.get('irDevice');
-    var carrier = this.config.get('irCarrier');
-    var cooldown = this.config.get('irCooldownMs');
-
-    if (dev) this.irDevice = dev;
-    if (carrier) this.irCarrier = String(carrier);
-    if (typeof cooldown === 'number') this.irCooldownMs = cooldown;
-
-    return libQ.resolve();
-};
-
-amphandler.prototype.onStart = function () {
-    return libQ.resolve();
-};
-
-amphandler.prototype.onStop = function () {
-    return libQ.resolve();
-};
-
-amphandler.prototype.onRestart = function () {
     return libQ.resolve();
 };
 
@@ -53,24 +36,17 @@ amphandler.prototype.getUIConfig = function () {
     var self = this;
     var lang_code = this.commandRouter.sharedVars.get('language_code');
 
-    self.commandRouter
-        .i18nJson(
-            __dirname + '/i18n/strings_' + lang_code + '.json',
-            __dirname + '/i18n/strings_en.json',
-            __dirname + '/UIConfig.json'
-        )
-        .then(function (uiconf) {
-            defer.resolve(uiconf);
-        })
-        .fail(function () {
-            defer.reject(new Error());
-        });
+    self.commandRouter.i18nJson(
+        __dirname + '/i18n/strings_' + lang_code + '.json',
+        __dirname + '/i18n/strings_en.json',
+        __dirname + '/UIConfig.json'
+    ).then(function (uiconf) {
+        defer.resolve(uiconf);
+    }).fail(function () {
+        defer.reject(new Error());
+    });
 
     return defer.promise;
-};
-
-amphandler.prototype.getConfigurationFiles = function () {
-    return ['config.json'];
 };
 
 amphandler.prototype._sendIrFile = function (filename) {
@@ -93,52 +69,14 @@ amphandler.prototype._sendIrFile = function (filename) {
 
             if (err) {
                 self.logger.error('[amphandler] IR send failed (' + filename + '): ' + (stderr || err.message || err));
-                defer.resolve();
-                return;
+                return defer.resolve();
             }
 
-            self.logger.info('[amphandler] IR sent: ' + filename);
             defer.resolve();
         }
     );
 
     return defer.promise;
-};
-
-amphandler.prototype.setUIConfig = function (data) {
-    var self = this;
-
-    if (!data) return libQ.resolve();
-
-    var key = Object.keys(data).find(function (k) {
-        return data[k] === true;
-    });
-
-    if (!key) return libQ.resolve();
-
-    var map = {
-        power: 'power.ir',
-        volUp: 'vol_up.ir',
-        volDown: 'vol_down.ir',
-        linePhono: 'line_phono.ir',
-        lineCd: 'line_cd.ir',
-        lineOne: 'line_one.ir',
-        lineTwo: 'line_two.ir'
-    };
-
-    var file = map[key];
-    if (!file) return libQ.resolve();
-
-    return self._sendIrFile(file);
-};
-
-amphandler.prototype.getConf = function (varName) {
-    return this.config.get(varName);
-};
-
-amphandler.prototype.setConf = function (varName, varValue) {
-    this.config.set(varName, varValue);
-    return libQ.resolve();
 };
 
 amphandler.prototype.sendIr = function (data) {
@@ -147,17 +85,7 @@ amphandler.prototype.sendIr = function (data) {
     var action = data && data.action ? data.action : null;
     if (!action) return libQ.resolve();
 
-    var map = {
-        power: 'power.ir',
-        volUp: 'vol_up.ir',
-        volDown: 'vol_down.ir',
-        linePhono: 'line_phono.ir',
-        lineCd: 'line_cd.ir',
-        lineOne: 'line_one.ir',
-        lineTwo: 'line_two.ir'
-    };
-
-    var file = map[action];
+    var file = self.irMap[action];
     if (!file) return libQ.resolve();
 
     return self._sendIrFile(file);
